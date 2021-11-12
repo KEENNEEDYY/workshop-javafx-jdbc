3653e6b03c51cccd222dccd827e78a05182f6593
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2021-11-12 08:55:01 -0300

    SellerForm

--- src/module-info.java
+++ src/module-info.java
@@ -7,7 +7,7 @@ module teste {
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml, javafx.controls;
-	opens gui.util to javafx.fxml, javafx.controls;
+	opens gui.util to javafx.fxml, javafx.controls, javafx.base;
 	opens model.entities to javafx.fxml, javafx.controls, javafx.base;
-	opens model.services to javafx.fxml, javafx.controls;
+	opens model.services to javafx.fxml, javafx.controls, javafx.base;
 }
